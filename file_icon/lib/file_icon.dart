8be83c42f4e4f3c7811cee815179ed963d58f452
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2021-06-09 07:40:25 -0400

    Added static methods to expose IconData and Icon

--- file_icon/lib/file_icon.dart
+++ file_icon/lib/file_icon.dart
@@ -5,12 +5,11 @@ class FileIcon extends StatelessWidget {
   final String fileName;
   final double size;
 
-  FileIcon(String fileName, {this.size})
+  FileIcon({required String fileName, required this.size})
       : this.fileName = fileName.toLowerCase();
 
-  @override
-  Widget build(BuildContext context) {
-    String key;
+  static String getExtension({required String fileName, String? fallback}) {
+    String key = '';
 
     if (iconSetMap.containsKey(fileName)) {
       key = fileName;
@@ -26,17 +25,46 @@ class FileIcon extends StatelessWidget {
       }
     }
 
-    if (key == null) {
-      key = '.txt';
+    if (key == '' && fallback != null) {
+      key = fallback;
     }
+    return key;
+  }
 
+  static IconData getIconData({required String fileName, String? fallback}) {
+    var key = getExtension(fileName: fileName, fallback: '.txt');
+    return _getIconDataForKey(key: key);
+  }
+
+  static IconData _getIconDataForKey({required String key}) {
+    return _getIconDataForCodePoint(iconSetMap[key]!.codePoint);
+  }
+
+  static IconData _getIconDataForCodePoint(int codePoint) {
+    return IconData(codePoint, fontFamily: 'Seti', fontPackage: 'file_icon');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return getIcon(fileName: this.fileName, size: this.size);
+  }
+
+  static Icon getIcon({required String fileName, required double size}) {
+    var key = getExtension(fileName: fileName, fallback: '.txt');
     return Icon(
-      IconData(
-        iconSetMap[key].codePoint,
-        fontFamily: 'Seti',
-        fontPackage: 'file_icon',
-      ),
-      color: Color(iconSetMap[key].color),
+      _getIconDataForKey(key: key),
+      color: Color(iconSetMap[key]!.color),
+      size: size,
+    );
+  }
+
+  static IconData getFolderIconData() {
+    return _getIconDataForCodePoint(0xE02F);
+  }
+
+  static Icon getFolderIcon({required double size}) {
+    return Icon(
+      getFolderIconData(),
       size: size,
     );
   }
